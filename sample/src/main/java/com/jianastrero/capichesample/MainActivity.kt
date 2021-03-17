ff5abe33f5b7924fa4a3d0da86cd02f21534c71b
Jian James Astrero <jian.astrero@e-hps.com>
2021-03-17 20:46:35 +0800

    Create a sample

--- sample/src/main/java/com/jianastrero/capichesample/MainActivity.kt
+++ sample/src/main/java/com/jianastrero/capichesample/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.jianastrero.capichesample
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.jianastrero.capiche.iNeed
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun requestCamera(view: View) {
+        iNeed(
+            Manifest.permission.CAMERA,
+            onGranted = {
+                tvCameraStatus.setText(R.string.camera_enabled)
+            },
+            onDenied = {
+                tvCameraStatus.setText(R.string.camera_disabled)
+            }
+        )
     }
 }
