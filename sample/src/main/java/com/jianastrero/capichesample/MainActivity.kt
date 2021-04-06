29596093210da536a767d70169053a637bedf227
Jian James Astrero <jianjamesastrero@gmail.com>
2021-04-06 22:02:59 +0800

    create doIHave sample

--- sample/src/main/java/com/jianastrero/capichesample/MainActivity.kt
+++ sample/src/main/java/com/jianastrero/capichesample/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import com.jianastrero.capiche.doIHave
 import com.jianastrero.capiche.iNeed
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,6 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doIHave(
+            Manifest.permission.CAMERA,
+            onGranted = {
+                tvCameraStatus.setText(R.string.camera_enabled)
+            },
+            onDenied = {
+                tvCameraStatus.setText(R.string.camera_disabled)
+            }
+        )
     }
 
     fun requestCamera(view: View) {
